a1ee0102a3a2a4739f8a891e42757c1f1589a4c5
Marguerite Kennedy <mkennedy@enviroissues.com>
2023-06-16 14:06:15 -0700

    master index and css changes

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
       })
       .then(data => {
         document.querySelector("#header-placeholder").innerHTML = data;
+        window.scrollTo(0, 0); // Force scroll to top
       });
 
     fetch("footer.html")
@@ -14,4 +15,26 @@ window.addEventListener('DOMContentLoaded', (event) => {
       .then(data => {
         document.querySelector("#footer-placeholder").innerHTML = data;
       });
-});
+
+       // Get the current page's URL
+       var url = window.location.href;
+  
+       // Extract the page name from the URL
+       var pageName = url.substring(url.lastIndexOf('/') + 1);
+      
+       // Remove the .html extension
+       pageName = pageName.split('.').shift();
+      
+       // Get the nav item with the id that corresponds to the page name
+       var navItem = document.getElementById(pageName);
+      
+       // Add the active-nav-item class to this nav item
+       if (navItem) {
+         navItem.classList.add('active-nav-item');
+       }
+      
+      });
+      
+
+
+
