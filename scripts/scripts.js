138a9cc32db79d33291f5859b64fc0638c87e6ba
CarlosPrieto <carlosprieto66@icloud.com>
2023-08-11 17:45:14 -0700

    fixed accordion that stays open

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,3 +14,57 @@ function resizeHeaderImage() {
   
   window.addEventListener("resize", resizeHeaderImage);
   resizeHeaderImage();
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+    fetch("header.html")
+      .then(response => {
+        return response.text()
+      })
+      .then(data => {
+        document.querySelector("#header-placeholder").innerHTML = data;
+        window.scrollTo(0, 0); // Force scroll to top
+      });
+
+    fetch("footer.html")
+      .then(response => {
+        return response.text()
+      })
+      .then(data => {
+        document.querySelector("#footer-placeholder").innerHTML = data;
+      });
+
+       // Get the current page's URL
+       var url = window.location.href;
+  
+       // Extract the page name from the URL
+       var pageName = url.substring(url.lastIndexOf('/') + 1);
+      
+       // Remove the .html extension
+       pageName = pageName.split('.').shift();
+      
+       // Get the nav item with the id that corresponds to the page name
+       var navItem = document.getElementById(pageName);
+      
+       // Add the active-nav-item class to this nav item
+       if (navItem) {
+         navItem.classList.add('active-nav-item');
+       }
+      
+      });
+      
+
+  // #### Scrolls down to corresponding accordion when road-sign is clicked ###
+  document.addEventListener('DOMContentLoaded', () => {
+    const roadSignLinks = document.querySelectorAll('.road-sign a');
+    roadSignLinks.forEach((link) => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const target = link.getAttribute('href');
+        const accordionElement = document.querySelector(target);
+        if (accordionElement) {
+          accordionElement.parentElement.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    });
+  });
+  
